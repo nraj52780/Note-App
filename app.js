fac43994e18121050948a61fd0f370f14e04ff4b
nraj52780 <45739384+nraj52780@users.noreply.github.com>
2019-05-17 22:09:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,59 +1,69 @@
-const fs = require('fs');
-const _ = require('lodash');
-const yargs = require('yargs');
 
-const notes = require('./notes.js');
+const yargs = require('yargs')
+const notes = require('./notes.js')
 
-const titleOptions = {
-  describe: 'Title of note',
-  demand: true,
-  alias: 't'
-};
-const bodyOptions = {
-  describe: 'Body of note',
-  demand: true,
-  alias: 'b'
-};
-const argv = yargs
-  .command('add', 'Add a new note', {
-    title: titleOptions,
-    body: bodyOptions
-  })
-  .command('list', 'List all notes')
-  .command('read', 'Read a note', {
-    title: titleOptions,
-  })
-  .command('remove', 'Remove a note', {
-    title: titleOptions
-  })
-  .help()
-  .argv;
-var command = argv._[0];
 
-if (command === 'add') {
-  var note = notes.addNote(argv.title, argv.body);
-  if (note) {
-    console.log('Note created');
-    notes.logNote(note);
-  } else {
-    console.log('Note title taken');
-  }
-} else if (command === 'list') {
-  var allNotes = notes.getAll();
-  console.log(`Printing ${allNotes.length} note(s).`);
-  allNotes.forEach((note) => notes.logNote(note));
-} else if (command === 'read') {
-  var note = notes.getNote(argv.title);
-  if (note) {
-    console.log('Note found');
-    notes.logNote(note);
-  } else {
-    console.log('Note not found');
-  }
-} else if (command === 'remove') {
-  var noteRemoved = notes.removeNote(argv.title);
-  var message = noteRemoved ? 'Note was removed' : 'Note not found';
-  console.log(message);
-} else {
-  console.log('Command not recognized');
-}
+
+//  add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+//  remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.removeNote(argv.title)
+    }
+})
+
+// list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler() {
+        notes.listNotes()
+    }
+})
+
+// read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title)
+    }
+})
+
+yargs.parse()
